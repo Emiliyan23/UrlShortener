22fa69e76ea1838fb984ad3fb32c8380511d60a4
emili <emiliqn.slav@gmail.com>
2025-05-09 16:39:19 +0300

    Add rate limiting

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -1,5 +1,8 @@
+using System.Threading.RateLimiting;
+
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
+
 using UrlShortener.Infrastructure;
 using UrlShortener.Services;
 
@@ -15,12 +18,41 @@ builder.Services.AddCors(options =>
 {
 	options.AddPolicy("AllowVueFrontend", policy =>
 	{
-		policy.WithOrigins("https://emiliyanvasilev.net")
-			.WithOrigins("https://www.emiliyanvasilev.net")
-			.WithOrigins("http://localhost:5173")
+		policy.WithOrigins("https://emiliyanvasilev.net", "https://www.emiliyanvasilev.net", "http://localhost:5173")
 			.AllowAnyHeader()
 			.AllowAnyMethod();
 	});
+});
+
+builder.Services.AddRateLimiter(options =>
+{
+	options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+	{
+		if (context.Request.Method.Equals("OPTIONS", StringComparison.OrdinalIgnoreCase))
+		{
+			return RateLimitPartition.GetFixedWindowLimiter("OPTIONS", _ => new FixedWindowRateLimiterOptions
+			{
+				PermitLimit = int.MaxValue, // Allow nearly unlimited requests for OPTIONS
+				Window = TimeSpan.FromSeconds(1),
+				QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+				QueueLimit = 0,
+			});
+		}
+
+		string clientIp = context.Connection.RemoteIpAddress?.ToString() ?? "unknown";
+		return RateLimitPartition.GetFixedWindowLimiter(clientIp, key => new FixedWindowRateLimiterOptions
+		{
+			PermitLimit = 1,
+			Window = TimeSpan.FromMinutes(1),
+			QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+			QueueLimit = 0,
+		});
+	});
+	options.OnRejected = (context, token) =>
+	{
+		context.HttpContext.Response.StatusCode = 429;
+		return ValueTask.CompletedTask;
+	};
 });
 
 builder.Services.AddScoped<IShortenService, ShortenService>();
@@ -49,11 +81,14 @@ else
 	app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
 
 app.UseRouting();
 
 app.UseCors("AllowVueFrontend");
+
+app.UseRateLimiter();
 
 app.UseAuthorization();
 
